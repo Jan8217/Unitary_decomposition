23c9d8fe3a6f7f26fe49ef43dd209066fa62e5ee
Jan8217 <ge78xus@mytum.de>
2024-03-05 18:24:24 +0100

    20240306

--- dqas/combine_csv_dqas.py
+++ dqas/combine_csv_dqas.py
@@ -1,7 +1,7 @@
 import glob
 import pandas
 
-csv_list = glob.glob('C:/Users/yanzh/PycharmProjects/Unitary_decomposition/dqas/csv_files_for_matrix/dimension_qubit_3/new_operation_pool_09/*.csv')
+csv_list = glob.glob('C:/Users/yanzh/PycharmProjects/Unitary_decomposition/dqas/csv_files_for_matrix/dimension_qubit_3/new_operation_pool_10/*.csv')
 
 def format_pandas(df):
     df_new = df.iloc[:1].copy()
@@ -25,7 +25,7 @@ for file_i in csv_list[1:]:
 
 print(appended_csv)
 combined_csv_file_name = 'new_dataset_for_full_plots.csv'
-appended_csv.to_csv('C:/Users/yanzh/PycharmProjects/Unitary_decomposition/dqas/csv_files_for_matrix/dimension_qubit_3/new_operation_pool_09/combined_csv/'+combined_csv_file_name,index = False)
+appended_csv.to_csv('C:/Users/yanzh/PycharmProjects/Unitary_decomposition/dqas/csv_files_for_matrix/dimension_qubit_3/new_operation_pool_10/combined_csv/'+combined_csv_file_name,index = False)
 
 full_csv_file_name = 'new_dataset_for_full_plots_full.csv'
-full_append.to_csv('C:/Users/yanzh/PycharmProjects/Unitary_decomposition/dqas/csv_files_for_matrix/dimension_qubit_3/new_operation_pool_09/combined_csv/'+full_csv_file_name,index = False)
+full_append.to_csv('C:/Users/yanzh/PycharmProjects/Unitary_decomposition/dqas/csv_files_for_matrix/dimension_qubit_3/new_operation_pool_10/combined_csv/'+full_csv_file_name,index = False)
